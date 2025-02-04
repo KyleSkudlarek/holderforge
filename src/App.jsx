5c2a746de7b091278d29c23cd3d17561136775bb
Kyle <kjs2wp@virginia.edu>
2025-02-03 20:33:23 -0600

    deleting unused BoxPreview  MILESTONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Provider } from "jotai";
-import BoxPreview from "./BoxPreview";
 import GridPreview from "./GridPreview";
 
 function App() {
   return (
     <Provider>
-        {/* <BoxPreview /> */}
         <GridPreview />
     </Provider>
   );

5ebbf7f0c0f82f8eb9704936a1afe2e44fc47f0a
Kyle <kjs2wp@virginia.edu>
2025-02-05 15:43:40 -0600

    adding footer min height on  mobile  MILESTONE

--- src/GridPreview.jsx
+++ src/GridPreview.jsx
@@ -43,7 +43,7 @@ const GridLayout = styled.div`
       /* Mobile */
   @media (max-width: ${breakpoints.mobile}) {
     grid-template-columns: 1fr;
-    grid-template-rows: auto auto 1fr auto auto;
+    grid-template-rows: auto auto 1fr auto 50px;
     grid-template-areas:
       "header"
       "left"
